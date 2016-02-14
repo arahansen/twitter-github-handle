49d0b5ad95fc1194290466b6d609b744db34b1de
Andrew Hansen <arahan@uw.edu>
2016-02-14 12:56:42 -0800

    check for route changes

--- index.js
+++ index.js
@@ -1,71 +1,82 @@
 (function() {
-  var githubUrl;
-  var user = {
-    handle: '',
-    firstname: '',
-    lastname: '',
-  };
+  var currentUrl = window.location.href;
+  main();
+  setInterval(function() {
+    if (window.location.href !== currentUrl) {
+      main();
+      currentUrl = window.location.href;
+    }
+  }, 250);
 
-  var userElem = document.getElementsByClassName('ProfileHeaderCard-screennameLink')[0];
+  function main() {
+    var githubUrl;
+    var user = {
+      handle: '',
+      firstname: '',
+      lastname: '',
+    };
 
-  if (userElem) {
-    var userNameElem = document.getElementsByClassName('ProfileHeaderCard-nameLink')[0];
-    var userName   = userNameElem.text.split(' ');
+    var userElem = document.getElementsByClassName('ProfileHeaderCard-screennameLink')[0];
 
-    user.firstname = userName[0];
-    user.lastname  = userName[1];
-    user.handle = userElem.text.substr(1);
+    if (userElem) {
+      var userNameElem = document.getElementsByClassName('ProfileHeaderCard-nameLink')[0];
+      var userName   = userNameElem.text.split(' ');
 
-    var githubUser = fetchGithubUser(user);
+      user.firstname = userName[0];
+      user.lastname  = userName[1];
+      user.handle = userElem.text.substr(1);
 
-    if (githubUser) {
-      githubUrl = githubUser.html_url;
-    } else {
-      return;
+      var githubUser = fetchGithubUser(user);
+
+      if (githubUser) {
+        githubUrl = githubUser.html_url;
+      } else {
+        return;
+      }
+
+      var githubContainer = document.createElement('span');
+      githubContainer.innerText = ' | ';
+
+      var githubLogo = buildLogo();
+      var githubLink = buildLink(user.handle, githubUrl);
+
+      githubContainer.appendChild(githubLogo);
+      githubContainer.appendChild(githubLink);
+      userElem.parentElement.insertBefore(githubContainer, userElem.nextSibling);
     }
 
-    var githubContainer = document.createElement('span');
-    githubContainer.innerText = ' | ';
+    function buildLink(text, url) {
+      var link = document.createElement('a');
+      link.innerText = user.handle;
+      link.href = githubUrl;
 
-    var githubLogo = buildLogo();
-    var githubLink = buildLink(user.handle, githubUrl);
-
-    githubContainer.appendChild(githubLogo);
-    githubContainer.appendChild(githubLink);
-    userElem.parentElement.insertBefore(githubContainer, userElem.nextSibling);
-  }
-
-  function buildLink(text, url) {
-    var link = document.createElement('a');
-    link.innerText = user.handle;
-    link.href = githubUrl;
-
-    return link;
-  }
-
-  function buildLogo() {
-    var logo = document.createElement('img');
-    logo.src = 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png';
-    logo.width  = 20;
-    logo.height = 20;
-    logo.style  = 'transform: translate(-2px, 5px)';
-
-    return logo;
-  }
-
-  function fetchGithubUser(user) {
-    var result;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", 'https://api.github.com/search/users?q=' + user.firstname + '+' + user.lastname, false);
-    xhr.send();
-
-    if (xhr.status === 200) {
-      result = JSON.parse(xhr.responseText);
+      return link;
     }
 
-    return result && result.total_count >= 1
-      ? result.items[0]
-      : undefined;
+    function buildLogo() {
+      var logo = document.createElement('img');
+      logo.src = 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png';
+      logo.width  = 20;
+      logo.height = 20;
+      logo.style  = 'transform: translate(-2px, 5px)';
+
+      return logo;
+    }
+
+    function fetchGithubUser(user) {
+      var result;
+      var xhr = new XMLHttpRequest();
+
+      xhr.open("GET", 'https://api.github.com/search/users?q=' + user.firstname + '+' + user.lastname, false);
+      xhr.send();
+
+      if (xhr.status === 200) {
+        result = JSON.parse(xhr.responseText);
+      }
+
+      return result && result.total_count >= 1
+        ? result.items[0]
+        : undefined;
+    }
   }
 })();
